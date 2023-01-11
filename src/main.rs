164fd317e5eaf024e9bcafd6cee80919300dd208
Ryan Ruckley <rruckley@gmail.com>
2023-01-11 14:11:30 +1100

    sample NER code.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
 fn main() {
     println!("Hello, world!");
+
+    let ner_model = NERModel::new(default::default())?;
+
+    let input = [
+        "My name is Amy. I live in Paris.",
+        "Paris is a city in France."
+    ];
+
+    let output = ner_model.predict(&input);
 }
