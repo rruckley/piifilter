0a2fc8ed69621a7471dd195f82b623747bbb191b
Ryan Ruckley <ryan.ruckley@optus.com.au>
2023-01-16 12:13:55 +1100

    Basic workings

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,7 @@ use rocket::http::ContentType;
 
 mod docs;
 use docs::Document;
+use tokio::task;
 
 
 #[macro_use] extern crate rocket;
@@ -30,41 +31,88 @@ struct InputData {
 #[post("/process", data = "<form_data>")]
 async fn process(form_data : Form<InputData>) -> (ContentType,String) {
     let action = &form_data.action;
-    let result = format!("<html><h2>All Good with action {}</h2></html>",action); 
-    (ContentType::HTML,result)
+    let result = match action.as_str() {
+        "regex" => process_regex(form_data.text.clone()).await,
+        "nep" => process_ner(form_data.text.clone()).await,
+        "pos" => process_pos(form_data.text.clone()).await,
+        _ => Ok(format!("Invalid Action: {}",action).to_owned())
+    };
+    (ContentType::HTML,result.unwrap())
 }
 
-fn process_text(input : &[&str], context : String) -> Result<String,NotFound<String>> {
+///! Function to execute regex against supplied text to find PII identifiers
+async fn process_regex(context : String) -> Result<String,String> {
+    Ok("Regex Not implemented".to_owned())
+}
 
-    /***
-    let input = [
-        "My name is Amy. I live in Paris.",
-        "Paris is a city in France.",
-        "My Medicare number is 0234903 and my email is ryan.ruckley@optus.com.au",
-        "I want to cancel my phone 042349839, my passport is 2340934",
-        "We have given soluiton walkthrough (Sukumar) and then Mona has shared the reasoning for the http used within the docker",
-        "Richard Branson, founder of Virgin Galactic, is now offering manned space flights for as little as $200,000."
-    ];
-    ***/
-
+fn _holder(context : String) -> Result<String,String> {
     let ner_model = NERModel::new(Default::default()).expect("Could not create NER model");
 
-    let output = ner_model.predict_full_entities(input);
+    let input = [context.as_str()];
+    let output = ner_model.predict_full_entities(&input);
 
     for entity in output {
         println!("{:?}",entity);
     };
-    // PoS tagging
+    Ok("Filtering NER".to_owned())
+}
+
+fn filter_ner(context : String) -> Result<String,String> {
+    let ner_model = NERModel::new(Default::default()).expect("Could not create NER model");
+
+    let input = [context.as_str()];
+    let output = ner_model.predict_full_entities(&input);
+
+    let mut result = "<html><h2>POS Output</h2><body>".to_owned();
+    result.push_str("<ul>");
+    for tag in output {
+        for t in tag {
+            result.push_str("<li>");
+            result.push_str(t.label.as_str());
+            result.push_str(" : ");
+            result.push_str(t.word.as_str());
+            result.push_str("</li>");
+        }
+    }
+    result.push_str("</ul></body></html>");
+    Ok(result)
+}
+
+async fn process_ner(context : String) -> Result<String,String> {
+    let result = task::spawn_blocking(move || {
+        filter_ner(context)
+    }).await.unwrap();
+    Ok(format!("<html><h2>NER All Good</h2><p>{}</p></html>",result.expect("Error with NER model")).to_owned())
+}
+
+fn filter_pos(context : String) -> Result<String,String> {
     let pos_model = POSModel::new(Default::default()).expect("Couldnt create PoS model");
 
-    let input = ["My name is Ryan Ruckley and I am an architect."];
+    let input = [context];
     let output = pos_model.predict(&input);
 
-    for (pos, pos_tag) in output[0].iter().enumerate() {
-        println!("{} - {:?}",pos, pos_tag);
+    let mut result = "<html><h2>POS Output</h2><body>".to_owned();
+    result.push_str("<ul>");
+    for tag in output {
+        for t in tag {
+            result.push_str("<li>");
+            result.push_str(t.label.as_str());
+            result.push_str(" : ");
+            result.push_str(t.word.as_str());
+            result.push_str("</li>");
+        }
     }
+    result.push_str("</ul></body></html>");
 
-    Ok("<html><h2>All Good</h2></html>".to_owned())
+    Ok(result)
+}
+
+async fn process_pos(context : String) -> Result<String,String> {
+  // PoS tagging
+  let result = task::spawn_blocking(move || {
+    filter_pos(context)
+  }).await.unwrap();
+  Ok(format!("Process POS: {}",result.unwrap()).to_owned())
 }
 
 #[get("/")]
@@ -75,8 +123,14 @@ async fn index() -> (ContentType, &'static str) {
     <body>
     <h2>PII Filter</h2>
     <form method=\"post\" action=\"/process\">
-    <textarea name=\"text\"></textarea>
-    <input name=\"action\" />
+    <textarea name=\"text\" rows=\"10\" cols=\"64\"></textarea>
+    <br />
+    <select name=\"action\">
+    <option value=\"regex\">Regular Expressions</option>
+    <option value=\"nep\">Named Entity Parsing</option>
+    <option value=\"pos\">Parts of Speech Tagging</option>
+    </select>
+    <br />
     <input type=\"submit\" />
     </form>
     </body>
