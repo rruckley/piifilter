92a0c8b138e7f6be9673774517d2639e0b172b74
Ryan Ruckley <ryan.ruckley@optus.com.au>
2023-01-19 14:40:31 +1100

    Added SIM

--- src/docs.rs
+++ src/docs.rs
@@ -14,6 +14,7 @@ pub enum DocType {
     DriverLicense,
     ForeignPassport,
     Medicare,
+    ICCID,
 }
 
 impl std::fmt::Display for DocType {
@@ -26,6 +27,7 @@ impl std::fmt::Display for DocType {
             Self::DriverLicense => write!(f,"Drivers License"),
             Self::ForeignPassport => write!(f,"Foreign Passport"),
             Self::Medicare => write!(f,"Medicare Cart"),
+            Self::ICCID => write!(f,"SIM Card"),
 
         }
     }
@@ -55,6 +57,7 @@ impl Document {
             DocType::DriverLicense => Regex::new("DRIVER").unwrap(),
             DocType::ForeignPassport  => Regex::new("FPASS").unwrap(),
             DocType::Medicare => Regex::new("\\d{4}-\\d{5}-\\d").unwrap(),
+            DocType::ICCID => Regex::new("\\d{13}").unwrap(),
         }
     }
 }
