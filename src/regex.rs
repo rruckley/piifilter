5590d1e1519b2c1cd11ea8a7bba30056663e10a0
Ryan Ruckley <ryan.ruckley@optus.com.au>
2023-01-25 10:34:51 +1100

    Added MSN Using SPANs

--- src/regex.rs
+++ src/regex.rs
@@ -1,8 +1,14 @@
 
 use crate::docs::{Document,DocType};
+use regex::*;
 
 pub struct RegexFilter {
     docs : Vec<Document>,
+}
+
+struct RegexMatch<'a> {
+    doc_type : String,
+    matched : Match<'a>,
 }
 
 impl RegexFilter {
@@ -15,7 +21,8 @@ impl RegexFilter {
         let driver = Document::new(DocType::DriverLicense, 50);
         let medicare = Document::new(DocType::Medicare, 30);
         let iccid = Document::new(DocType::Iccid, 0);
-        let docs = vec![cpass,epass,fpass,bcert,ccert,driver,medicare,iccid];
+        let msn = Document::new(DocType::MobileServiceNumber, 10);
+        let docs = vec![cpass,epass,fpass,bcert,ccert,driver,medicare,iccid,msn];
         Self {
             docs,
         }
@@ -24,17 +31,26 @@ impl RegexFilter {
     /// Iterate through context looking for all document identifiers
     pub fn filter(&self, context : String) -> Result<String,String> {
         let mut output = context.clone();
+        let mut all_matches = vec![];
         for doc in self.docs.iter() {
             // Look for regex matches
             let matches = doc.pattern.find_iter(context.as_str());
-            for m in matches {
-                //output.push_str(format!("{} : From {} to {}",doc.doc_type,m.start(),m.end()).as_str());
-                let start = m.start();
-                let end = m.end();
-                let fill = "X".repeat(end-start);
-                //let span = format!("<span class=\"{}\">{}</span>",doc.doc_type,fill);
-                output.replace_range(start..end, &fill)
-            }
+            matches.for_each(|m| {
+                all_matches.push(RegexMatch {
+                    doc_type : doc.doc_type.to_string(),
+                    matched : m,
+                });
+            })
+          
+        }
+        // Need to iterate in reverse so that we don't invalidate the offsets
+        for m in all_matches.iter().rev() {
+            //output.push_str(format!("{} : From {} to {}",doc.doc_type,m.start(),m.end()).as_str());
+            let start = m.matched.start();
+            let end = m.matched.end();
+            let fill = "X".repeat(end-start);
+            let span = format!("<span class=\"{}\">{}</span>",m.doc_type,fill);
+            output.replace_range(start..end, &span)
         }
         
         Ok(format!("<div class=\"regex\">{}</div>",output))
