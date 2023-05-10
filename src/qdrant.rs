bba17ad80e9582caf5d6eca9b4c78b6117fe97bd
Ryan Ruckley <ryan.ruckley@optus.com.au>
2023-05-10 15:44:31 +1000

    new module

---
+++ src/qdrant.rs
@@ -0,0 +1,27 @@
+// API into QDrant vector database
+
+#[derive(Debug)]
+pub struct QDrantPoint {
+    pub id : i64,
+    pub payload : String,
+    pub vector  : Vec<f32>,
+}
+
+#[derive(Debug)]
+pub struct QDrantPoints {
+    pub points : Vec<QDrantPoint>,
+}
+
+#[derive(Debug)]
+pub struct QDrant {
+    pub instance : String,
+}
+
+impl QDrant {
+    pub fn store(points : QDrantPoints) -> Result<String,String> {
+        let count = points.points.len();
+        info!("Storing {count} points");
+        dbg!(points);
+        Ok("Not implemented".to_string())
+    }
+}
