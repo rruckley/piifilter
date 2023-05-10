056b0ccfd8be2e94d9d8af48bef1ec2437d91bd9
Ryan Ruckley <ryan.ruckley@optus.com.au>
2023-05-10 17:23:45 +1000

    Now push to db

--- src/qdrant.rs
+++ src/qdrant.rs
@@ -1,13 +1,18 @@
 // API into QDrant vector database
+use serde::Serialize;
 
-#[derive(Debug)]
+#[derive(Debug,Serialize)]
+pub struct QDrantPayload {
+
+}
+#[derive(Debug,Serialize)]
 pub struct QDrantPoint {
-    pub id : i64,
-    pub payload : String,
+    pub id : String,
+    pub payload : QDrantPayload,
     pub vector  : Vec<f32>,
 }
 
-#[derive(Debug)]
+#[derive(Debug,Serialize)]
 pub struct QDrantPoints {
     pub points : Vec<QDrantPoint>,
 }
@@ -15,12 +20,30 @@ pub struct QDrantPoints {
 #[derive(Debug)]
 pub struct QDrant {
     pub instance : String,
+    pub collection : String,
 }
 
 impl QDrant {
-    pub fn store(points : QDrantPoints) -> Result<String,String> {
+    pub fn new(instance : String,collection : String) -> Self {
+        Self {
+            instance,
+            collection,
+        }
+    }
+    pub async fn store(&self,points : QDrantPoints) -> Result<String,String> {
         let count = points.points.len();
         info!("Storing {count} points");
-        Ok("Not implemented".to_string())
+        let req = reqwest::Client::new();
+        let body = serde_json::to_string(&points).unwrap();
+        let url = format!("{}/collections/{}/points",self.instance,self.collection);
+        match req.put(url).body(body).send().await {
+            Ok(r) => {
+                Ok(r.text().await.unwrap())
+            },
+            Err(e) => {
+                error!("Could not save points: {e}");
+                Err(e.to_string())
+            },
+        }
     }
 }
