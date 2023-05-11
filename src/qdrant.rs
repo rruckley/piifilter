2e0619f79cbccf534a3d79756a75dc5bb7902a18
Ryan Ruckley <ryan.ruckley@optus.com.au>
2023-05-11 11:30:43 +1000

    added search

--- src/qdrant.rs
+++ src/qdrant.rs
@@ -1,20 +1,55 @@
 // API into QDrant vector database
-use serde::Serialize;
+use serde::{Serialize, Deserialize};
 
 #[derive(Debug,Serialize)]
 pub struct QDrantPayload {
 
 }
+
+type QDrantVector = Vec<f32>;
 #[derive(Debug,Serialize)]
 pub struct QDrantPoint {
     pub id : String,
     pub payload : QDrantPayload,
-    pub vector  : Vec<f32>,
+    pub vector  : QDrantVector,
 }
 
 #[derive(Debug,Serialize)]
 pub struct QDrantPoints {
     pub points : Vec<QDrantPoint>,
+}
+
+#[derive(Debug,Serialize)]
+pub struct QDrantParams {
+    hnsw_ef : i32,
+    exact   : bool,
+}
+
+#[derive(Debug,Serialize)]
+pub struct QDrantFilter {
+
+}
+
+#[derive(Debug,Serialize)]
+pub struct QDrantSearch {
+    filter  : QDrantFilter,
+    params  : QDrantParams,
+    vector  : QDrantVector,
+    with_payload    : bool,
+    limit   : i32,
+}
+
+#[derive(Debug,Deserialize)]
+pub struct QDrantScoredVec {
+    id  : String,
+    score   : f32,
+}
+
+#[derive(Debug,Deserialize)]
+pub struct QDrantResponse {
+    result : Vec<QDrantScoredVec>,
+    status : String,
+    time   : f32,
 }
 
 #[derive(Debug)]
@@ -43,6 +78,34 @@ impl QDrant {
             Err(e) => {
                 error!("Could not save points: {e}");
                 Err(e.to_string())
+            },
+        }
+    }
+
+    pub async fn search(&self, vec : Vec<f32>) -> Result<Vec<String>,String> {
+        let req = reqwest::Client::new();
+        let url = format!("{}/collections/{}/points/search",self.instance,self.collection);
+        let filter = QDrantSearch {
+            filter : QDrantFilter {  },
+            params : QDrantParams { hnsw_ef: 128, exact: false },
+            vector : vec,
+            with_payload : true,
+            limit : 5,
+        };
+        let body = serde_json::to_string(&filter).expect("Could not parse QDrant response");
+        match req.post(url).body(body).send().await {
+            Ok(r) => {
+                info!("Results from Qdrant!");
+                let response : QDrantResponse = serde_json::from_str(r.text().await.unwrap().as_str()).unwrap();
+                let mut output : Vec<String> = vec![];
+                for v in response.result {
+                    output.push(v.id);
+                }
+                Ok(output)
+            }
+            Err(e) => {
+                error!("Could not search DB: {e}");
+                Err(e.to_string()) 
             },
         }
     }
